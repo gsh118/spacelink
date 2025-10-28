675e6af96f158d3d6b27900ff0ea93d48816270f
gsh118 <hks01108@naver.com>
2025-10-28 23:38:59 +0900

    chore: 스프링 시큐리티 의존성 추가 및 백엔드 cors 설정 (#11)

---
+++ backend/spacelink/src/main/java/com/gsh118/spacelink/config/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.gsh118.spacelink.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+// 아직 정식 시큐리티 설정은 하지 않음
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(requests->requests
+                        .anyRequest().permitAll())
+                .cors(cors->cors
+                        .configurationSource(corsConfigurationSource()));
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5741"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
